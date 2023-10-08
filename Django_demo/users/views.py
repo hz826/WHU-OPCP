5d5c2cfd2d117b6e300e4bcabf8d708a3126efa6
Miko2333 <liuxirui03@hotmail.com>
2023-10-08 23:16:17 +0800

    Added first Django demo

---
+++ Django_demo/users/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.views.generic import ListView, CreateView
+
+
+def index(request):
+    return render(request, 'users/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        _username = request.POST['username']
+        _password = request.POST['password']
+        user = User.objects.create_user(
+            username=_username,
+            password=_password
+        )
+        return HttpResponseRedirect(reverse('users:index'))
+    return render(request, 'users/register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        _username = request.POST['username']
+        _password = request.POST['password']
+        user = authenticate(username=_username, password=_password)
+        if user is not None:
+            return render(request, 'users/index.html', {"username":_username})
+    return render(request, 'users/login.html')
