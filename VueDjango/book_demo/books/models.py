8d88d329e8d4b26e4e1bba84b0618e6abacbf18b
MikukuOvO <mikukuovo@gmail.com>
2023-10-26 14:31:39 +0800

    Remove last version

--- VueDjango/book_demo/books/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Books(models.Model):
-    name = models.CharField(max_length=30)
-    author = models.CharField(max_length=30, blank=True, null=True)
