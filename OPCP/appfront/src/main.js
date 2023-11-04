4034494fa2e32a6886f8c67a7f2bd5a60070079d
MikukuOvO <mikukuovo@gmail.com>
2023-11-04 11:04:46 +0800

    Add file upload for the front connect successfully

--- OPCP/appfront/src/main.js
+++ OPCP/appfront/src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Vuex from 'vuex'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 // import Cookies from 'js-cookie'
 
 Vue.use(Vuex)
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
--- OPCP/appfront/src/views/HomeView.vue
+++ OPCP/appfront/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <HelloWorld /> 
+    <input style="width: 74px;" type="file" id="uFile" name="uFile" @change="upload($event)"/>
   </div>
 </template>
 
@@ -12,6 +13,32 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    upload(e){
+      var that=this
+      var files = document.getElementById('uFile').value;
+      if (!/\.(gif|jpg|jpeg|png|gif|jpg|png)$/i.test(files)) {
+        this.$message.warning("图片类型必须是.gif,jpeg,jpg,png中的一种,请重新上传")
+        return false;
+      }
+      let file = e.target.files[0]    
+      let formData = new FormData()
+      formData.append('filename', 'mdlxr');
+      formData.append('file', file);
+      this.axios.post(`http://localhost:8000/api/upload/`, formData).then((response)=>{   
+ 
+              this.$message.success("添加成功")  //需要引入elemrnt
+      }).catch((error)=>{
+          console.log(error)
+              this.$message.warning("添加失败")
+        })
+     },
   }
 }
 </script>
