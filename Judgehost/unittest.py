f911c94a0d2bf7eaa0aeaeaa223f5624da46ff24
hz826 <2945994284@qq.com>
2023-11-14 22:01:14 +0800

    增加了五子棋的测试代码

--- Judgehost/unittest.py
+++ Judgehost/unittest.py
@@ -7,9 +7,11 @@ config = {
 }
 
 os.system('rm -rf run/')
-print(judge(config, 'judge/tic_tac_toe.cpp', [['code/unittest/tic_tac_toe/random', "cpp_single_gcc_std17"], ['code/unittest/tic_tac_toe/py', "python3_single"]]))
-print(judge(config, 'judge/tic_tac_toe.cpp', [['code/unittest/tic_tac_toe/random', "cpp_single_gcc_std17"], ['code/unittest/tic_tac_toe/mle', "cpp_single_gcc_std17"]]))
-print(judge(config, 'judge/tic_tac_toe.cpp', [['code/unittest/tic_tac_toe/random', "cpp_single_gcc_std17"], ['code/unittest/tic_tac_toe/tle', "cpp_single_gcc_std17"]]))
+print(judge(config, 'judge/gomoku.cpp', [['code/unittest/gomoku/gomoku1', "cpp_single_gcc_std17"], ['code/unittest/gomoku/gomoku1', "cpp_single_gcc_std17"]]))
+
+# print(judge(config, 'judge/tic_tac_toe.cpp', [['code/unittest/tic_tac_toe/random', "cpp_single_gcc_std17"], ['code/unittest/tic_tac_toe/py', "python3_single"]]))
+# print(judge(config, 'judge/tic_tac_toe.cpp', [['code/unittest/tic_tac_toe/random', "cpp_single_gcc_std17"], ['code/unittest/tic_tac_toe/mle', "cpp_single_gcc_std17"]]))
+# print(judge(config, 'judge/tic_tac_toe.cpp', [['code/unittest/tic_tac_toe/random', "cpp_single_gcc_std17"], ['code/unittest/tic_tac_toe/tle', "cpp_single_gcc_std17"]]))
 # print(judge(config, 'judge/tic_tac_toe.cpp', [['code/unittest/tic_tac_toe/random', "cpp_single_gcc_std17"], ['code/unittest/tic_tac_toe/best', "cpp_single_gcc_std17"]]))
 # print(judge(config, 'judge/tic_tac_toe.cpp', [['code/unittest/tic_tac_toe/random', "cpp_single_gcc_std17"], ['code/unittest/tic_tac_toe/random', "cpp_single_gcc_std17"]]))
 # print(judge(config, 'judge/tic_tac_toe.cpp', [['code/unittest/tic_tac_toe/random', "cpp_single_gcc_std17"], ['code/unittest/tic_tac_toe/re1', "cpp_single_gcc_std17"]]))
